2d3d171e5ac5c51493e7a7d688217167696661f9
GitHub Action <action@github.com>
2022-09-13 14:08:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/adgod/myplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/adgod/myplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.adgod.myplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adgod.myplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/adgod/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adgod/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.adgod.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adgod.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
